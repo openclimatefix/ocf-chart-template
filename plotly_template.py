fbf3e25d6c1ba929d0952403889acb65efd7e46d
Megawattz <47150349+zakwatts@users.noreply.github.com>
2024-02-29 11:52:51 +0000

    Create default plotly styling

---
+++ plotly_template.py
@@ -0,0 +1,11 @@
+import plotly.io as pio
+
+colors = ['#7BCDF3', '#63BCAF', '#FF9736', '#FFD053' , '#14120E', '#4C9A8E']  # Define a custom color palette for plots
+pio.templates.default = "plotly"  # Set the default plotly template for consistent styling
+pio.templates["plotly"]["layout"]["font"] = dict(family="Inter")  # Set the default font family to 'Inter'
+pio.templates["plotly"]["layout"]["colorway"] = colors  # Apply the custom color palette to the plot colorway
+pio.templates["plotly"]["layout"]["title"] = dict(x=0.5)  # Center the plot title horizontally
+pio.templates["plotly"]["layout"]["margin"] = dict(t=50)  # Reduces the top margin to bring the title closer to the graphs
+pio.templates["plotly"]["layout"]["plot_bgcolor"] = "#FFF"  # Set the background color of the plot to white
+pio.templates["plotly"]["layout"]["xaxis"]["gridcolor"] = '#E4E4E4'  # Set the grid color of the x-axis (light grey)
+pio.templates["plotly"]["layout"]["yaxis"]["gridcolor"] = '#E4E4E4'  # Set the grid color of the y-axis (light grey)
